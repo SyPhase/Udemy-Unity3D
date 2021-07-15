887af084c9f948ee6c7a163933ed3d78e46d5cc2
SyPhase <pi2568gamerz@gmail.com>
2021-07-15 19:32:38 +0200

    made levels
    -SFX for thrust
    -tags and collision
    -continues to next level upon completion + loops back to level 1 when finished with all levels

--- 3_Project_Boost/Rusty Rocket/Assets/Scripts/Movement.cs
+++ 3_Project_Boost/Rusty Rocket/Assets/Scripts/Movement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    AudioSource audioSource;
     [SerializeField] float upwardThrust = 1f;
     [SerializeField] float rotationThrust = 1f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -24,6 +26,15 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(0, upwardThrust, 0);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
@@ -45,4 +56,18 @@ public class Movement : MonoBehaviour
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         rb.freezeRotation = false; // Unfreezing rotation to allow physics interactions
     }
+
+
+    ////////// EXPERIMENT //////////
+        // causes rocket to not rotate on contact
+    /*
+    void OnCollisionStay(Collision other) // EXPERIMENT WITH PHYSICS
+    {
+        rb.freezeRotation = true; // Freezing rotation to manually rotate
+    }
+    void OnCollisionExit(Collision other)
+    {
+        rb.freezeRotation = false; // Unfreezing rotation to allow physics interactions
+    }
+    */    
 }
