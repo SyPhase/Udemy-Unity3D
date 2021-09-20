96b3d84de46345856d728e3afdba543c50b5a2b6
SyPhase <pi2568gamerz@gmail.com>
2021-09-20 18:49:20 +0200

    made difficulty increase as game progresses
    -enemy HP will increase each time they are killed

--- 5_Realm_Rush/Invasion of Cubes/Assets/Enemy/enemyHealth.cs
+++ 5_Realm_Rush/Invasion of Cubes/Assets/Enemy/enemyHealth.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class enemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHP = 5;
-    [SerializeField] int currentHP = 0;
+    [Tooltip("Adds amount to maxHP when enemy dies")]
+    [SerializeField] int difficultyRamp = 1;
+
+    int currentHP = 0;
 
     Enemy enemy;
 
@@ -30,6 +34,7 @@ public class enemyHealth : MonoBehaviour
         if (currentHP < 1)
         {
             enemy.RewardGold();
+            maxHP += difficultyRamp;
             gameObject.SetActive(false);
         }
     }
