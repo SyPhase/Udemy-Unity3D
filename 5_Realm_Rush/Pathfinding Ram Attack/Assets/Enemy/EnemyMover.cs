7ec6d10256bc619d6a615ed23277a0840fb0ca85
SyPhase <pi2568gamerz@gmail.com>
2021-09-23 17:09:08 +0200

    started new pathfinding system

---
+++ 5_Realm_Rush/Pathfinding Ram Attack/Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>(); // list to hold each waypoint
+    [SerializeField] [Range(0f, 10f)] float speed = 1f; // speed of enemies on path
+
+    Enemy enemy;
+
+    void OnEnable()
+    {
+        FindPath();
+        ReturnToStart();
+        StartCoroutine(FollowPath());
+    }
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    void FindPath() // located the parent empty tagged with "path" then adds each child as waypoint
+    {
+        path.Clear();
+
+        GameObject parent = GameObject.FindGameObjectWithTag("Path");
+
+        foreach(Transform child in parent.transform)
+        {
+            Waypoint waypoint = child.GetComponent<Waypoint>();
+
+            if (waypoint != null)
+            {
+                path.Add(waypoint);
+            }
+        }
+    }
+
+    void ReturnToStart()
+    {
+        transform.position = path[0].transform.position;
+    }
+
+    void FinishPath()
+    {
+        enemy.StealGold();
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        FinishPath();
+    }
+}
